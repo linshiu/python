409da201ee92585b0c59882355be5b354b53dfe8
linshiu <slin10.uy@gmail.com>
2015-08-14 20:55:11 -0700

    first commit

--- data_structures_algorithms/problem_solving_data_structures_algorithms/chap05_searching_sorting/shellSort.py
+++ data_structures_algorithms/problem_solving_data_structures_algorithms/chap05_searching_sorting/shellSort.py
@@ -3,7 +3,7 @@
 Problem Solving with Algorithms and Data Structures, Brad Miller
 Chapter 5: Searching and Sorting
 IDE: Spyder, Python 3
-Selection Sort
+Shell Sort
 """
 import timeit
 import matplotlib.pyplot as plt
@@ -12,13 +12,10 @@ import numpy as np
 
 #%% Function #################################################################
 
-def insertionSort(ls):
-    """ Sort a list using selection sort
-    
-    Make passes through the list until list is sorted. Assume start with a 
-    sublist with only the first item. In each pass, compare the next item 
-    in the list to the ones in the sublist, and shift items right until correct
-    position found to insert item.
+def shellSort(ls):
+    """ Sort a list using shell sort. The idea is to break down the list 
+    into sublists with items apart by a gap. Sort each sublist and repeat
+    by changing the gap until the sublist = fulllist (gap = 1).
     
     Args:
         ls (list): list to sort
@@ -27,24 +24,55 @@ def insertionSort(ls):
         None: modifies the list
         
     """    
-    
+
     n = len(ls)
     
-    # passes (up to n-1) 
-    for nPass in range(n-1):
+    gap = n // 2  # number of sublists = gap
+    
+    # iterate until number of sublists = 1
+    while gap > 0:
+        
+        # iterate each sublist
+        for start in range (gap):  # start = starting point of sublist
+            insertionSortGap(ls, start, gap) # sort sublist
+        
+        gap = gap // 2 # reduce the number of sublists
 
-        # iterate until nPass-1 comparisons in sublist or found insertion location
+def insertionSortGap(ls, start, gap):
+    """ Sort sublist using insertion sort
+    
+    Make passes through the sublist until sublist is sorted. Assume start with a 
+    sublist with only the first item. In each pass, compare the next item 
+    in the list to the ones in the sublist, and shift items right until correct
+    position found to insert item.
+    
+    Args:
+        ls (list): full list
+        start (int): index of starting point of sublist
+        gap (int): 
+    
+    Returns:
+        None: modifies the list
+
+    """    
+    n = len(ls)
+    
+    # passes 
+    # iterate sublist in increments = gap (stop n-gap since getting start + gap)
+    for nPass in range(start,n-gap,gap):
+        
+        # iterate until comparisons done in sublist or found insertion location
         i = nPass
-        new = ls[nPass + 1]
+        new = ls[nPass + gap]
              
-        while i >= 0 and new < ls[i]:
+        while i >= start and new < ls[i]:
             
             # shift to the right
-            ls[i+1] = ls[i]
+            ls[i+gap] = ls[i]
             
-            i-=1
+            i-=gap
         
-        ls[i+1] = new
+        ls[i+gap] = new
 
 #%%  Testl #################################################################
 def test():
@@ -61,7 +89,7 @@ def test():
     testList1 = [54,26,93,17,77,31,44,55,20]
     testList2 = [54,26,93,17,77,31,44,55,20]
     
-    insertionSort(testList1)
+    shellSort(testList1)
     testList2.sort()
     
     assert testList1 == testList2
@@ -73,12 +101,12 @@ if __name__ == "__main__":
     test()
         
   
-    func= [insertionSort]
+    func= [shellSort]
     times = {f.__name__: [] for f in func}
     
     for f in times:
         
-        for n in range(5):
+        for n in range(3):
             size = 5**(n+1)
             item = nprnd.randint(size*3, size = size)
             testList = nprnd.randint(low=0, high = size, size = size)
